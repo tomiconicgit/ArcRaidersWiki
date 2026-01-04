e7e66b654b309be394350129459fd26ead7b4ecc
tomiconicgit <tomiconicx@icloud.com>
2026-01-04 13:30:46 +0000

    Delete js/main.js

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import { Background } from './modules/Background.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Initialize the 3D Background
-    const bg = new Background('canvas-container');
-
-    console.log("Arc Raiders Hub: Modules Loaded Successfully.");
-});
